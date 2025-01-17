46a34b999fdeb735d8d1fcfe1469bfca06b762db
HariVkm <167500728+HariVkm@users.noreply.github.com>
2025-01-17 20:26:06 +0530

    Add three numbers using Typescript

---
+++ add.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var console_1 = require("console");
+var prompt = require('prompt-sync')();
+var a = Number(prompt('Enter the first number:-'));
+var b = Number(prompt('Enter the second number:-'));
+var c = Number(prompt('Enter the second number:-'));
+function addNumbers(a, b, c) {
+    return (a + b + c);
+}
+var sum = addNumbers(a, b, c);
+(0, console_1.log)('Sum of two numbers is: ' + sum);
